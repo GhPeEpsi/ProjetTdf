55333bde191c75b50cb35787cfc59a4ed6488eb3
Noé <noe.guillouet@gmail.com>
2018-09-24 17:30:36 +0200

    Ajout de PremierTestConn.php et .html permettant de se connecter à la base avec un liste déroulante

---
+++ php/PremierTestConn.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="fr">
+	<head>
+		<meta charset="utf-8">
+		<title>Test de connexion + Liste déroulante</title>
+	</head>
+	<body>
+		<?php
+		include ("pdo_oracle.php");
+		include ("util_affichage.php");
+	
+		$login = 'copie_tdf';
+		$mdp = 'copie_tdf_local';
+		$db = 'oci:dbname=localhost:1521/xe';
+	
+		// ce code ne doit pas être dans le <select> … </select>
+		$conn = OuvrirConnexion($db,$login,$mdp);
+		$req = 'SELECT * FROM vt_coureur order by nom';
+		$nbLignes = LireDonnees1($conn,$req,$tab);
+		
+		if (!empty($_POST)) {
+			if (isset($_POST['coureur'])) {
+				$cour = $_POST['coureur'];
+				echo ("Coureur $cour sélectionné");
+			}
+		}
+		else {
+			include ("../html/PremierTestConn.html");
+		}
+		?>   	
+	</body>
+</html>
