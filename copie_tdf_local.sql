6fcb8fc0bb7947ae50a22eb17e73af0535f36fa8
Bastien <21700836@etu.unicaen.fr>
2018-09-24 10:10:41 +0200

    Premier commit avec fichier contenant les requètes sql + les fichier bootstrap à ne pas utiliser tout de suite ...

---
+++ copie_tdf_local.sql
@@ -0,0 +1,10 @@
+--Afficher tous les coureurs :
+select * from vt_coureur;
+select * from vt_coureur order by N_coureur;
+select * from vt_coureur order by Nom;
+select * from vt_coureur order by prenom;
+select * from vt_coureur order by annee_prem;
+
+
+
+select * from tdf_categorie_epreuve;
