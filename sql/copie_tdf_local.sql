62e774592e97deb7ce76b4b281377bb2b5aed7f3
Jérémy <FORz_DARKz@hotmail.fr>
2018-09-24 16:35:55 +0200

    organisation
    Organisation des dossiers

---
+++ sql/copie_tdf_local.sql
@@ -0,0 +1,17 @@
+--Afficher tous les coureurs :
+select * from vt_coureur;
+select * from vt_coureur order by N_coureur;
+select * from vt_coureur order by Nom;
+select * from vt_coureur order by prenom;
+select * from vt_coureur order by annee_prem;
+
+--Trouver un coureur en particulier :
+SELECT * from vt_coureur where Nom like 'A%'; -- nom changeable par prenom, n_coureur, annee_prem
+SELECT * from vt_coureur where Nom like '%A%';
+SELECT * from vt_coureur where Nom like '%A';
+
+--vérification si un coureur à participé ou nom au TDF :
+select * from vt_coureur
+
+
+select * from tdf_categorie_epreuve;
