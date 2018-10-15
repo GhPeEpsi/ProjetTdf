888704de356f3b98ba98b0919f37bf1cf80b3c0e
Prunier <21700836@etu.unicaen.fr>
2018-10-16 00:35:04 +0200

    Avancement sur le l'affichage des infos dont la place en fonction du tour

--- php/affichageCoureur.php
+++ php/affichageCoureur.php
@@ -9,26 +9,38 @@
 		include ("pdo_oracle.php");
 		include ("util_affichage.php");
 		
+		/*Serveur UNICAEN
 		$login = 'ETU2_49';
 		$mdp = 'ETU2_49';
 		$db = fabriquerChaineConnexion();
 		$conn = OuvrirConnexion($db,$login,$mdp);
+		*/
+		/*Bastien Localhost*/
+		$login = 'projet_php';
+		$mdp = 'projet_php';
+		$db = fabriquerChaineConnexion2();
+		$conn = OuvrirConnexion($db,$login,$mdp);
+		/**/
 		
 		// --n_coureur, nom, prenom, nation, ... :
 		$reqBase = 
-			"select n_coureur as "Numero de coureur", co.nom as "Nom", prenom as "Prenom", annee_naissance as "Annee de naissance", annee_prem as "Annee de première", na.nom as "Nation"
+			"select n_coureur as \"Numero de coureur\", co.nom as \"Nom\", prenom as \"Prenom\", annee_naissance as \"Annee de naissance\", annee_prem as \"Annee de première\", na.nom as \"Nation\"
 			from tdf_coureur co
 			join tdf_app_nation using (n_coureur)
 			join tdf_nation na using (code_cio)
-			where co.nom = \'JOACHIM\'
-			and prenom = \'Benoit\'";
+			where co.nom = 'JOACHIM'
+			and prenom = 'Benoit'";
 		$nbLignesBase = LireDonnees1($conn,$reqBase,$tabBase);
 		
 		$reqNbParticipation = 
-			"select count(*) from tdf_parti_coureur
+			"select count(*) as \"Nombre de participation au tdf\" from tdf_parti_coureur
 			join tdf_coureur using (n_coureur)
 			where nom = 'JOACHIM'
 			and prenom = 'Benoit'";
+		$nbLignesNb = LireDonnees1($conn,$reqNbParticipation,$tabNb);
+		
+		$reqPlaceParticipation = 
+			"select * from tdf_coureur";
 		$nbLignesNb = LireDonnees1($conn,$reqNbParticipation,$tabNb);
 			
 		$req = 'SELECT * FROM tdf_coureur order by nom';
@@ -37,17 +49,21 @@
 		
 
 		//PLACE D'ARRIVÉE A CHAQUE TOUR
-		// Remplissage des info si coureur séléctionné :
-		
-		echo '<pre>';
+		//Remplissage des info si coureur séléctionné :
 		function afficheBase() {
 			global $nbLignesBase, $tabBase;
-			print_r($tabBase);
+			foreach ($tabBase[0] as $key => $ligne)
+				echo "<p>$key : $ligne</p>";
 		}
 		
 		function afficheNb() {
 			global $nbLignesNb, $tabNb;
-			print_r($tabNb);
+			foreach($tabNb[0] as $key => $ligne)
+				echo "<p>$key : $ligne</p>";
+		}
+		
+		function afficherPlace() {
+			global $nbLignesNb, $tabNb;
 		}
 		
 		//Le fichier html:
--- sql/copie_tdf_local.sql
+++ sql/copie_tdf_local.sql
@@ -104,5 +104,20 @@ Insert into tdf_app_nation(n_coureur, code_cio,annee_debut) values ((select max(
     join tdf_coureur using (n_coureur)
     where nom = 'JOACHIM'
     and prenom = 'Benoit';
-    --équipe :
-
+    --place à chaque tour :
+    select * from tdf_parti_coureur;
+    select * from tdf_ordrequi order by numero_ordre, annee;
+    select * from tdf_coureur co
+    join tdf_parti_coureur using(N_coureur)
+    join tdf_sponsor using (n_equipe, n_sponsor)
+    join tdf_ordrequi using (n_equipe, n_sponsor, annee)
+    where co.nom = 'JOACHIM'
+    and prenom = 'Benoit';
+    --final
+    select annee, numero_ordre from tdf_coureur co
+    join tdf_parti_coureur using(N_coureur)
+    join tdf_sponsor using (n_equipe, n_sponsor)
+    join tdf_ordrequi using (n_equipe, n_sponsor, annee)
+    where co.nom = 'JOACHIM'
+    and prenom = 'Benoit';
+    
