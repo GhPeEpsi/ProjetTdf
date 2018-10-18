843a2406c1296dda558ca60ee333aab0ccccb303
Noé <noe.guillouet@gmail.com>
2018-10-18 16:10:23 +0200

    resolution du probleme d'affichage dans la liste deroulante des nationalites, maintenant il affiche toutes la nationalites et selectionne la nationalite du coureur selectionne, il n'y a plus besoin de se faire chier, ça m'a pris un temps fou, parce que je suis mauvais en php et bastien m'a bien aide mais j'en ai rien a cirer

--- php/modificationCoureur.php
+++ php/modificationCoureur.php
@@ -17,23 +17,48 @@
 	include ("../html/modificationCoureur.html");
 
 	function getNumeroCoureur($conn) {
-		$req1 = 'SELECT n_coureur FROM tdf_coureur where n_coureur = \''.$_GET['numCoureur'].'\'';
-		$nbLignes1 = LireDonnees1($conn,$req1,$tab);
+		$req = 'SELECT n_coureur FROM tdf_coureur where n_coureur = \''.$_GET['numCoureur'].'\'';
+		$nbLignes1 = LireDonnees1($conn,$req,$tab);
 		echo utf8_encode($tab[0]['N_COUREUR']);
 	}
 	
 	function getNomCoureur($conn) {
-		$req1 = 'SELECT nom FROM tdf_coureur where n_coureur = \''.$_GET['numCoureur'].'\'';
-		$nbLignes1 = LireDonnees1($conn,$req1,$tab);
+		$req = 'SELECT nom FROM tdf_coureur where n_coureur = \''.$_GET['numCoureur'].'\'';
+		$nbLignes1 = LireDonnees1($conn,$req,$tab);
 		echo utf8_encode($tab[0]['NOM']);
 	}
 	
 	function getPrenomCoureur($conn) {
-		$req1 = 'SELECT prenom FROM tdf_coureur where n_coureur = \''.$_GET['numCoureur'].'\'';
-		$nbLignes1 = LireDonnees1($conn,$req1,$tab);
+		$req = 'SELECT prenom FROM tdf_coureur where n_coureur = \''.$_GET['numCoureur'].'\'';
+		$nbLignes1 = LireDonnees1($conn,$req,$tab);
 		echo utf8_encode($tab[0]['PRENOM']);
 	}
-	
+
+	function getPaysCoureur($conn) {
+		$req = 'SELECT tdf_nation.nom FROM tdf_nation
+				JOIN tdf_app_nation USING (code_cio)
+				WHERE n_coureur = \''.$_GET['numCoureur'].'\'';
+		$nbLignes1 = LireDonnees1($conn,$req,$tab);
+		return utf8_encode($tab[0]['NOM']);
+	}
+
+	function getListePays($conn) {
+		$req = 'SELECT code_cio, nom FROM tdf_nation where annee_disparition is null order by nom';
+		$nbLignes = LireDonnees1($conn,$req,$tab);
+		$paysCoureur = getPaysCoureur($conn);
+		for ($i=0; $i<$nbLignes; $i++) {
+			if($tab[$i]['NOM'] == $paysCoureur) {
+				$tab[$i]["NOM"] = utf8_encode($tab[$i]["NOM"]);
+				echo '<option value="'.$tab[$i]['CODE_CIO'].'" selected>'.$tab[$i]['NOM'];
+				echo '</option>';
+			} else {
+				$tab[$i]["NOM"] = utf8_encode($tab[$i]["NOM"]);
+				echo '<option value="'.$tab[$i]['CODE_CIO'].'">'.$tab[$i]['NOM'];
+				echo '</option>';
+			}
+		}
+	}
+
 	function listeCoureurs($tab,$nbLignes) {
 		for ($i=0; $i<$nbLignes; $i++) {
 			$tab[$i]["PRENOM"] = utf8_encode($tab[$i]["PRENOM"]);
