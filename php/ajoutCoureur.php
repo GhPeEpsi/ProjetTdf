d0a8c626564bfc7eb41d0cec30421d5bf8a56252
Bastien <21700836@etu.unicaen.fr>
2018-11-05 12:48:56 +0100

    réindentation ajout coureur

--- php/ajoutCoureur.php
+++ php/ajoutCoureur.php
@@ -3,30 +3,27 @@
 	include ("util_affichage.php");
 	include ("verificationsForm.php");
 	//echo '<meta charset="utf-8">';
-	
+
 	// connexion à la base
 	//$db_username = 'ETU2_49';
 	//$db_password = 'ETU2_49';
 	//$db = "oci:dbname=spartacus.iutc3.unicaen.fr:1521/info.iutc3.unicaen.fr;charset=AL32UTF8";
 
 	//connection de Jérémy qui resté là après la merge
-	$db_username = 'copie_tdf_copie';
-	$db_password = 'copie_tdf_copie';
-	$db = "oci:dbname=localhost:1521/xe;charset=AL32UTF8";
+	//$db_username = 'copie_tdf_copie';
+	//$db_password = 'copie_tdf_copie';
+	//$db = "oci:dbname=localhost:1521/xe;charset=AL32UTF8";
 	//$db = fabriquerChaineConnexion();
-	
-	
-	// $db_username = 'copie_tdf';
-	// $db_password = 'copie_tdf';
-	// $db = fabriquerChaineConnexion2();
+
+
+	$db_username = 'copie_tdf';
+	$db_password = 'copie_tdf';
+	$db = fabriquerChaineConnexion2();
 	$conn = OuvrirConnexion($db,$db_username,$db_password);
-	//$conn->exec("set names AL32UTF8");
 
 	//récupérer seulement l'annee de la date entrée
 	if(isset($_GET['dateN'])){
 		$dateN = $_GET['dateN'];
-		//$tab1 = explode("-", $dateN);
-		//$annee = $tab1[0];
 		echo $dateN;
 	}
 
@@ -34,7 +31,7 @@
 		$nom = $_POST['Nom'];
 		$nom = testNom($nom, $regex);
 	}
-	
+
 	if(isset($_POST['prenom'])){
 		$prenom = $_POST['prenom'];
 		$prenom = testPrenom($prenom, $regex);
@@ -52,66 +49,30 @@
 
 	$req = 'SELECT code_cio, nom FROM tdf_nation where annee_disparition is null order by nom';
 	$nbLignes = LireDonnees1($conn,$req,$tab);
-	// condition pour que rien ne se passe si tout n'est pas rempli, sinon, ajout du coureur à la base grace à la requête
 	
+	// condition pour que rien ne se passe si tout n'est pas rempli, sinon, ajout du coureur à la base grace à la requête
 	if(isset($_POST['verifier'])){
 
-		if (empty($_POST['Nom']) || empty($_POST['prenom']) || $_POST['nationalite'] == ''){ //!isset($_POST['dateN']) || || !verifDepuisQ(recupAnnee())
+		//verfication du bon remplissage des champs obligatoire :
+		if (empty($_POST['Nom']) || empty($_POST['prenom']) || $_POST['nationalite'] == ''){
 			echo "<script> alert('vous n\'avez pas tout rempli') </script>";
-
 		}else{
-			//Changement de la valeur de l'element hidden :
-			
-		
-
-			//BLOC 1
-
+			//verification de la validité des nom, prenom :
 			if($nom == NULL || $prenom == NULL){
-				
+
 				if($nom == NULL){
 					echo "<script> alert('Le nom entré n\'est pas valide, recommencer'); </script>";
-					//echo $prenom;
-
-					//$_POST['nationalite'] = $nat;
-					//echo "<br>";
 				}
-				
+
 				if($prenom == NULL){
 					echo "<script> alert('Le prenom entré n\'est pas valide, recommencer'); </script>";
-					//echo $nom;
-					//$_POST['Nom'] = $nom;
-					//$_POST['nationalite'] = $nat;
-					//echo "<br>";
 				}
 
 			}else{
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 				if(!empty($_POST['dateN']) && empty($_POST['depuisQ'])){
 					$verifInt = $_POST['dateN'];
+					
 					if(!ctype_digit($verifInt)|| $verifInt < 1900 || $verifInt > date('Y')){
 						echo("Vous n'avez pas entré une date valide");
 					}else{
@@ -119,48 +80,33 @@
 						$depuisQuand = $annee_naissance;
 						$sql = "INSERT INTO tdf_coureur(n_coureur, nom, prenom, annee_naissance) VALUES ((select max(n_coureur) from tdf_coureur) + 1, :nom, :prenom, :annee_naissance)";
 						$cur = preparerRequete($conn,$sql);
-						//AfficherTab($cur);
-						//FIN BLOC 1
 
-
-						//BLOC 2 
 						ajouterParam($cur,':nom',$nom);
 						ajouterParam($cur,':prenom',$prenom);
 						ajouterParam($cur,':annee_naissance',$annee_naissance);
+						$passage = false;
 						if (nonExistant()) {
 							$res = majDonneesPreparees($cur);
+							$passage = true;
 						}
-						else {
-							echo "<p>Le coureur existe deja</p>";
-							echo '<input type="submit" name="continuer" value="Continuer" >';
-							return;
-						}
-						//AfficherTab($res);
-						//FIN BLOC 2
-
+						
 						//requête pour ajouter annee_debut à la table tdf_app_nation en fonction du depuisQ rentré
 						$sql2 = "INSERT INTO tdf_app_nation(n_coureur, code_cio,annee_debut) VALUES ((select max(n_coureur) from tdf_coureur),:nat, :depuisQuand)";
 
 						$cur = preparerRequete($conn,$sql2);
-						//AfficherTab($cur);
-						//FIN BLOC 1
-
-						//BLOC 2 
 						ajouterParam($cur,':nat',$nat);
 						ajouterParam($cur,':depuisQuand',$depuisQuand);
-						if (nonExistant()) {
+						if ($passage) {
 							$res = majDonneesPreparees($cur);
 						}
-						else {
-							echo "<p>Le coureur existe deja</p>";
-							echo '<input type="submit" name="continuer" value="Continuer" >';
-							return;
-						}
-						//AfficherTab($res);
 
-						echo "Vous avez inséré le coureur ".$nom. " " .$prenom." de nationalité ".$nat;
+						if ($passage) {
+							echo "Vous avez inséré le coureur ".$nom. " " .$prenom." de nationalité ".$nat;
+						}
+						else {
+							echo "<p>Le coureur existe deja veuillez changer le nom, le prenom, ou la nationalité</p>";
+						}
 					}
-					//echo "Vous devez remplir depuis quand si vous insérez une date de naissance";
 				}elseif(!empty($_POST['dateN'])&& (!empty($_POST['depuisQ']))){
 					$verifInt = $_POST['dateN'];
 					if(!ctype_digit($verifInt)|| $verifInt < 1900 || $verifInt > date('Y')){
@@ -168,142 +114,99 @@
 					}else{
 						$annee_naissance = recupAnnee();
 						$depuisQuand = intval($_POST['depuisQ']);
-					// echo $annee_naissance;
-					// echo $depuisQuand;
-					// echo gettype($depuisQuand);
-					// echo gettype($annee_naissance);
 
 						if($depuisQuand > $annee_naissance){
-						echo "Vérifier que l'année entrée dans \"depuis Quand\" est inférieure à l'année de naissance";
+							echo "Vérifier que l'année entrée dans \"depuis Quand\" est inférieure à l'année de naissance";
 						}else{
-						//requête pour ajouter un coureur à la base.
-						$sql = "INSERT INTO tdf_coureur(n_coureur, nom, prenom, annee_naissance) VALUES ((select max(n_coureur) from tdf_coureur) + 1, :nom, :prenom, :annee_naissance)";
-						$cur = preparerRequete($conn,$sql);
-						//AfficherTab($cur);
-						//FIN BLOC 1
+							//requête pour ajouter un coureur à la base.
+							$sql = "INSERT INTO tdf_coureur(n_coureur, nom, prenom, annee_naissance) VALUES ((select max(n_coureur) from tdf_coureur) + 1, :nom, :prenom, :annee_naissance)";
+							$cur = preparerRequete($conn,$sql);
+							 
+							ajouterParam($cur,':nom',$nom);
+							ajouterParam($cur,':prenom',$prenom);
+							ajouterParam($cur,':annee_naissance',$annee_naissance);
+							$passage = false;
+							if (nonExistant()) {
+								$res = majDonneesPreparees($cur);
+								$passage = true;
+							}
 
+							//requête pour ajouter annee_debut à la table tdf_app_nation en fonction du depuisQ rentré
+							$sql2 = "INSERT INTO tdf_app_nation(n_coureur, code_cio,annee_debut) VALUES ((select max(n_coureur) from tdf_coureur),:nat, :depuisQuand)";
 
-						//BLOC 2 
-						ajouterParam($cur,':nom',$nom);
-						ajouterParam($cur,':prenom',$prenom);
-						ajouterParam($cur,':annee_naissance',$annee_naissance);
-						
-						if (nonExistant()) {
-							$res = majDonneesPreparees($cur);
-						}
-						//AfficherTab($res);
-						//FIN BLOC 2
+							$cur = preparerRequete($conn,$sql2);
 
-						//requête pour ajouter annee_debut à la table tdf_app_nation en fonction du depuisQ rentré
-						$sql2 = "INSERT INTO tdf_app_nation(n_coureur, code_cio,annee_debut) VALUES ((select max(n_coureur) from tdf_coureur),:nat, :depuisQuand)";
-
-						$cur = preparerRequete($conn,$sql2);
-						//AfficherTab($cur);
-						//FIN BLOC 1
-
-						//BLOC 2 
-						ajouterParam($cur,':nat',$nat);
-						ajouterParam($cur,':depuisQuand',$depuisQuand);
-						if (nonExistant()) {
-							$res = majDonneesPreparees($cur);
-						}
-						//AfficherTab($res);
-						if (nonExistant()) {
-							echo "Vous avez inséré le coureur ".$nom. " " .$prenom." de nationalité ".$nat;
-						}
-						else {
-							echo "<p>Le coureur existe deja veuillez changer le nom, le prenom, ou la nationalité</p>";
-						}
+							ajouterParam($cur,':nat',$nat);
+							ajouterParam($cur,':depuisQuand',$depuisQuand);
+							if ($passage) {
+								$res = majDonneesPreparees($cur);
+							}
+							
+							if ($passage) {
+								echo "Vous avez inséré le coureur ".$nom. " " .$prenom." de nationalité ".$nat;
+							}
+							else {
+								echo "<p>Le coureur existe deja veuillez changer le nom, le prenom, ou la nationalité</p>";
+							}
 						}
 					}
 				}else{
-						$annee_naissance = recupAnnee();
-						$depuisQuand = null;
+					$annee_naissance = recupAnnee();
+					$depuisQuand = null;
 					//requête pour ajouter un coureur à la base.
-						$sql = "INSERT INTO tdf_coureur(n_coureur, nom, prenom, annee_naissance) VALUES ((select max(n_coureur) from tdf_coureur) + 1, :nom, :prenom, :annee_naissance)";
-						$cur = preparerRequete($conn,$sql);
-						//AfficherTab($cur);
-						//FIN BLOC 1
+					$sql = "INSERT INTO tdf_coureur(n_coureur, nom, prenom, annee_naissance) VALUES ((select max(n_coureur) from tdf_coureur) + 1, :nom, :prenom, :annee_naissance)";
+					$cur = preparerRequete($conn,$sql); 
+					ajouterParam($cur,':nom',$nom);
+					ajouterParam($cur,':prenom',$prenom);
+					ajouterParam($cur,':annee_naissance',$annee_naissance);
+					
+					$passage = false;
+					if (nonExistant()) {
+						$res = majDonneesPreparees($cur);
+						$passage = true;
+					}
+					//requête pour ajouter annee_debut à la table tdf_app_nation en fonction du depuisQ rentré
+					$sql2 = "INSERT INTO tdf_app_nation(n_coureur, code_cio,annee_debut) VALUES ((select max(n_coureur) from tdf_coureur),:nat, :depuisQuand)";
 
+					$cur = preparerRequete($conn,$sql2);
+					ajouterParam($cur,':nat',$nat);
+					ajouterParam($cur,':depuisQuand',$depuisQuand);
+					
+					if ($passage) {
+						$res = majDonneesPreparees($cur);
+					}
 
-						//BLOC 2 
-						ajouterParam($cur,':nom',$nom);
-						ajouterParam($cur,':prenom',$prenom);
-						ajouterParam($cur,':annee_naissance',$annee_naissance);
-						if (nonExistant()) {
-							$res = majDonneesPreparees($cur);
-						}
-						else {
-							echo "<p>Le coureur existe deja</p>";
-							echo '<input type="submit" name="continuer" value="Continuer" >';
-							return;
-						}
-						
-						//AfficherTab($res);
-						//FIN BLOC 2
-
-						//requête pour ajouter annee_debut à la table tdf_app_nation en fonction du depuisQ rentré
-						$sql2 = "INSERT INTO tdf_app_nation(n_coureur, code_cio,annee_debut) VALUES ((select max(n_coureur) from tdf_coureur),:nat, :depuisQuand)";
-
-						$cur = preparerRequete($conn,$sql2);
-						//AfficherTab($cur);
-						//FIN BLOC 1
-
-						//BLOC 2 
-						ajouterParam($cur,':nat',$nat);
-						ajouterParam($cur,':depuisQuand',$depuisQuand);
-						if (nonExistant()) {
-							$res = majDonneesPreparees($cur);
-						}
-						else {
-							echo "<p>Le coureur existe deja</p>";
-							echo '<input type="button" name="continuer" >';
-							return;
-						}
-						//AfficherTab($res);
-
+					if ($passage) {
 						echo "Vous avez inséré le coureur ".$nom. " " .$prenom." de nationalité ".$nat;
+					}
+					else {
+						echo "<p>Le coureur existe deja veuillez changer le nom, le prenom, ou la nationalité</p>";
+					}
 				}
-		 	}
+			}
 		}
 	}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+	//FUNCTION :
+	
 	//verifier que le coureur qu'on veut entrer n'existe pas deja :
 	function nonExistant() {
 		global $conn, $nom, $prenom, $nat;
-		
+
 		$req = 'select count(*) as nb from tdf_coureur 
-				join tdf_app_nation using (n_coureur)
-				where nom = \''.$nom.'\'
-				and prenom = \''.$prenom.'\'
-				and code_cio = \''.$nat.'\'';
-				
+		join tdf_app_nation using (n_coureur)
+		where nom = \''.$nom.'\'
+		and prenom = \''.$prenom.'\'
+		and code_cio = \''.$nat.'\'';
+
 		LireDonnees1($conn, $req, $tab);
-		
+
 		if ($tab[0]['NB'] == 0) {
 			return true;
 		}
 		return false;
 	}
 
-
-
-
-	
 	//permet d'aller voir les infos d'un coureur qui vient d'être entré :
 	if(isset($_POST['regarder'])){
 		if (isset($_POST['droitPassage']) && $_POST['droitPassage']=="true") {
@@ -324,24 +227,21 @@
 		}
 		echo "false";
 	}
-	
+
 	function recupAnnee(){
 		if(!empty($_POST['dateN'])){
-			 	$annee_naissance = $_POST['dateN'];
-	 			return intval($annee_naissance);
-	 		}
-	 	return null;
+			$annee_naissance = $_POST['dateN'];
+			return intval($annee_naissance);
+		}
+		return null;
 	}
 
-	
-	// On remplis la liste deroulante avec les nationalité de la base
 
-	function remplirOption($tab,$nbLignes) 
-	{
+	// On remplis la liste deroulante avec les nationalité de la base
+	function remplirOption($tab,$nbLignes) {
 		global $nat;
-		for ($i=0; $i<$nbLignes; $i++)
-		 {
-		 	if ($nat == $tab[$i]['CODE_CIO']) {
+		for ($i=0; $i<$nbLignes; $i++) {
+			if ($nat == $tab[$i]['CODE_CIO']) {
 				$tab[$i]["NOM"] = utf8_encode($tab[$i]["NOM"]);
 				echo '<option value="'.$tab[$i]['CODE_CIO'].'" selected>'.$tab[$i]['NOM'];
 				echo '</option>';
@@ -356,7 +256,6 @@
 
 	//retourne la nationalité sélectionnée
 	function ajoutSelection(){
-		
 		if (!empty($_POST)) {
 			if (isset($_POST['nationalite'])) {
 				$nat = $_POST['nationalite'];
@@ -367,31 +266,19 @@
 		}
 	}
 
-	// function verifDepuisQ(){
-	// 	global $annee_naissance;
-	// 	if(!empty($_POST['depuisQ']) && !empty($annee_naissance)){
-	// 		if((int)$_POST['depuisQ'] < (int)$annee_naissance){
-	// 			echo "Vérifier que l'année entrée est inférieure à l'année de naissance";
-	// 			return FALSE;
-	// 		}else{
-	// 			return TRUE;
-	// 		}
-	// 	} 
-	// }
-
 	function afficherNom(){
-		global $nom;
-		echo $nom;
+	global $nom;
+	echo $nom;
 	}
 
 	function afficherPrenom(){
-		global $prenom;
-		echo $prenom;
+	global $prenom;
+	echo $prenom;
 	}
 
 	function afficherDateN(){
-		global $dateNaissance;
-		echo $dateNaissance;
+	global $dateNaissance;
+	echo $dateNaissance;
 	}
 
 	function afficherDepuisQ(){
@@ -399,23 +286,9 @@
 		echo $depuisQuand;
 	}
 
-	// function listeAnnee(){
-	//   // Variable qui ajoutera l'attribut selected de la liste déroulante
-	//   $selected = '';
-	 
-	//   // Parcours du tableau
-	//   echo '<select name="dateN" onblur="chargerDate(document.getElementById(\'dateN\'))">',"\n";
-	//   for($i=1900; $i<= date('Y'); $i++)
-	//   {
-	//     // Affichage de la ligne
-	//     echo "\t",'<option value="">', $i ,'</option>',"\n";
-	//   }
-	//   echo '</select>',"\n";
-	// }
-
-
+	//insertion des 
 	if(empty($_GET)){
 		include ("../html/navBar.html");
-		include ("../html/ajoutCoureur.html"); //on inclut le fichier html
+		include ("../html/ajoutCoureur.html");
 	}
-	?>
+?>
