65ca885bcd85167eabab662d0da3341005502108
Noé <noe.guillouet@gmail.com>
2018-11-07 00:32:48 +0100

    affchageAbandon presque terminé

--- php/affichageAbandon.php
+++ php/affichageAbandon.php
@@ -1,8 +1,4 @@
 <?php
-    $req = 'SELECT annee, n_epreuve, n_coureur, libelle, ab.commentaire, ta.commentaire as raison
-            FROM tdf_abandon ab JOIN tdf_typeaban ta USING (c_typeaban)
-            WHERE annee = 2001 ORDER BY annee';
-
     include ("pdo_oracle.php");
     include ("util_affichage.php");
     include ("../html/navBar.html");
@@ -29,130 +25,66 @@
     }
 
     //REQUETE :
-    $reqLigne = 'select n_epreuve, nom, prenom, heure, minute, seconde, total_seconde, distance, jour from tdf_etape
-                join tdf_temps using (annee, n_epreuve)
-                join tdf_coureur using (n_coureur)
-                where annee = :annee
-                and rang_arrivee = 1
-                order by n_epreuve';
-    $curLigne = preparerRequete($conn,$reqLigne);
-
-    $reqNbVaiqueur = 'select n_epreuve, count(*) as nb from tdf_etape
-                    join tdf_temps using (annee, n_epreuve)
-                    where annee = :annee
-                    and rang_arrivee = 1
-                    group by n_epreuve
-                    order by n_epreuve';
-    $curNbVainqueur = preparerRequete($conn,$reqNbVaiqueur);
-
-
-    //FONCTIONS DE TRAITEMENT :
-    /*//Récupération de la liste des années
-    function listeAnnee() {
-        global $conn, $annee;
-        $req = "select annee from tdf_annee order by annee";
-        LireDonnees1($conn, $req, $tab);
-        
-        if (!empty($annee)) {echo '<option value="pasBon">Année</option>';}
-        else {echo '<option value="pasBon" selected>Année</option>';}
-        
-        foreach ($tab as $sousTab) {
-            $val = $sousTab['ANNEE'];
-            if (!empty($annee) && $val == $annee)
-                echo '<option value="'.$val.'" selected>'.$val.'</option>';
-            else
-                echo '<option value="'.$val.'">'.$val.'</option>';
-        }
-    }*/
+    $requete = 'SELECT n_epreuve, nom, prenom, libelle, aba.commentaire, typ.commentaire AS raison
+                FROM tdf_abandon aba
+                JOIN tdf_typeaban typ USING (c_typeaban)
+                JOIN tdf_coureur cou USING (n_coureur)
+                WHERE annee = :annee
+                ORDER BY n_epreuve';
+    $curseur = preparerRequete($conn,$requete);
 
     //affichage du tableau :
     function affichage() {
-        global $conn, $annee, $curLigne, $curNbVainqueur;
+        global $conn, $annee, $curseur, $requete;
         
-        //affichage du tableau quoi qu'il ce passe :
+        //affichage du tableau quoi qu'il se passe :
         $style = "style=\"border: 1px solid black;\"";
         echo "<table $style>";
         echo "<tr $style>
-                <th $style>N° Epreuve</th>
-                <th $style>Distance (en km)</th>
-                <th $style>Date</th>
-                <th $style>Gagnant</th>
-                <th $style>Temps</th>
-                <th $style>Temps (en s)</th>
-            </tr>";
+              <th $style> N° Epreuve </th>
+              <th $style> Coureur </th>
+              <th $style> Libellé </th>
+              <th $style> Commentaire </th>
+              <th $style> Raison de l'abandon </th>
+              </tr>";
         
         //affichage des données si une annee est entrée :
         if (isset($annee)) {
             //ajout de l'année aux requetes :
-            ajouterParam($curLigne,':annee',$annee);
-            ajouterParam($curNbVainqueur,':annee',$annee);
+            ajouterParam($curseur,':annee',$annee);
             
-            //résultat :
-            $nbEpreuve = LireDonneesPreparees($curLigne, $tabRes);
-            $nbVainqueur = LireDonneesPreparees($curNbVainqueur, $tabNb);
-
             //boucle d'affichage :
-            $j =0; //parcour du tableau de resultat
-            //afficheLigneTableau($tabNb, $style);
-            foreach ($tabNb as $etape) {
-                $tab = array();
-                for ($i = 0 ; $i<$etape['NB'] ; $i++) {
-                    $tab[] = $tabRes[$j];
-                    $j++;
-                }
+            $nbLignes = LireDonneesPreparees($curseur,$tab);
+            $tab = array();
+            for($i=0; $i<$nbLignes; $i++) {
                 afficheLigneTableau($tab, $style);
             }
         }
         else {
             echo "</table>";
-            echo "<p>Pas encore d'année selectionné !</p>";
+            echo "<p>Pas encore d'année selectionnée !</p>";
         }
-    }
-
-    function fabriqueCondition($EpMultiGagn) {
-        echo '<h1>coucou1</h1>';
-        $condition =
-        (
-            (
-                isset($epreuve[0]['N_EPREUVE'])
-                &&
-                ($epreuve[0]['N_EPREUVE'] != $EpMultiGagn)
-            )
-            || 
-            (
-                isset($epreuve['N_EPREUVE'])
-                &&
-                ($epreuve['N_EPREUVE'] != $EpMultiGagn)
-            )
-        );
-        
-        return $condition;
     }
 
     function afficheLigneTableau($tab, $style) {
-        /*echo '<pre>';
-        print_r($tab);
-        echo '</pre><br><br><br><br><br><br><br>';*/
         
         if (!isset($tab[1])) {
             echo '<tr '.$style.'>
-                <th '.$style.'>'.$tab[0]['N_EPREUVE'].'</th>
-                <th '.$style.'>'.$tab[0]['DISTANCE'].'</th>
-                <th '.$style.'>'.$tab[0]['JOUR'].'</th>
-                <th '.$style.'>'.utf8_encode($tab[0]['NOM']). ' ' . utf8_encode($tab[0]['PRENOM']).'</th>
-                <th '.$style.'>'.$tab[0]['HEURE']. 'h/' .$tab[0]['MINUTE']. 'min/' .$tab[0]['SECONDE'].'s</th>
-                <th '.$style.'>'.$tab[0]['TOTAL_SECONDE'].'</th>
-                </tr>';
+                  <th '.$style.'>'.$tab[0]['N_EPREUVE'].'</th>
+                  <th '.$style.'>'.utf8_encode($tab[0]['NOM']). ' ' . utf8_encode($tab[0]['PRENOM']).'</th>
+                  <th '.$style.'>'.$tab[0]['LIBELLE'].'</th>
+                  <th '.$style.'>'.$tab[0]['COMMENTAIRE'].'</th>
+                  <th '.$style.'>'.$tab[0]['RAISON'].'</th>
+                  </tr>';
         }
         else {
             echo '<tr '.$style.'>
-                <th '.$style.'>'.$tab[0]['N_EPREUVE'].'</th>
-                <th '.$style.'>'.$tab[0]['DISTANCE'].'</th>
-                <th '.$style.'>'.$tab[0]['JOUR'].'</th>
-                <th '.$style.'>'. afficheNomPrenom($tab).'</th>
-                <th '.$style.'>'.$tab[0]['HEURE']. 'h/' .$tab[0]['MINUTE']. 'min/' .$tab[0]['SECONDE'].'s</th>
-                <th '.$style.'>'.$tab[0]['TOTAL_SECONDE'].'</th>
-                </tr>';
+                  <th '.$style.'>'.$tab[0]['N_EPREUVE'].'</th>
+                  <th '.$style.'>'.afficheNomPrenom($tab).'</th>
+                  <th '.$style.'>'.$tab[0]['LIBELLE'].'</th>
+                  <th '.$style.'>'.$tab[0]['COMMENTAIRE'].'</th>
+                  <th '.$style.'>'.$tab[0]['RAISON'].'</th>
+                  </tr>';
         }
     }
 
@@ -166,6 +98,6 @@
     }
 
     //LE FICHIER HTML:
-    include("../html/affichageEtape.html");
+    include("../html/affichageAbandon.html");
 
 ?>
