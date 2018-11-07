648a00b18869d7214167352b66556918706e4885
Clément Catel <clement.catel4@gmail.com>
2018-11-07 13:15:27 +0100

    correction verif2

--- php/verificationsForm.php
+++ php/verificationsForm.php
@@ -32,8 +32,10 @@ function supprimeCaracteresSpeciaux($str, $encoding='UTF-8') {
     $str = preg_replace('#&([A-za-z])(?:slash|tilde);#', '\1', $str);
 
     // Supprimer les tirets en début et fin de chaine
-    $str = preg_replace('#^-|-$#', '', $str);
-
+    while (preg_match('#^-|-$#'), $str) {
+        $str = preg_replace('#^-|-$#', '', $str);
+    }
+    
     // Supprime les espaces après et/ou avant des tirets
     $str = preg_replace('# - |- | -#', '-', $str);
 
