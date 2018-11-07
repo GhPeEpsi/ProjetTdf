8a3f2a7b6cc5d2a00fc7b34097b64d7317743e97
Clément Catel <clement.catel4@gmail.com>
2018-11-07 13:04:35 +0100

    correction verifForm

--- php/verificationsForm.php
+++ php/verificationsForm.php
@@ -55,7 +55,7 @@ function testNom($nom, $regex) {
         }
 
         $array = preg_split("#--#", $nom);
-        if (count($array > 2)) {
+        if (count($array) > 2) {
             echo "<script> alert('Vous ne pouvez saisir de doubles-tirets qu'une fois !')</script>";
             return NULL;
         }
